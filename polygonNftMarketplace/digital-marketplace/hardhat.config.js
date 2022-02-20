a79cb3148ae04db0c017e6ba8ab56a2f71c73ac5
lyh <dltbwoddl@gmail.com>
2022-02-20 12:04:22 +0900

    2022-02-20

--- polygonNftMarketplace/digital-marketplace/hardhat.config.js
+++ polygonNftMarketplace/digital-marketplace/hardhat.config.js
@@ -1,25 +1,18 @@
-/* hardhat.config.js */
-require("@nomiclabs/hardhat-waffle")
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
 const { API_URL, PRIVATE_KEY } = process.env;
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 1337
-    },
-//  mumbai: {
-//    url: "https://rpc-mumbai.maticvigil.com",
-//    accounts: [privateKey]
-//  }
-  },
-  solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+   solidity: "^0.8.0",
+   defaultNetwork: "ropsten",
+   networks: {
+      hardhat: {},
+      ropsten: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
       }
-    }
-  }
+   },
 }
--- polygonNftMarketplace/digital-marketplace/scripts/deploy.js
+++ polygonNftMarketplace/digital-marketplace/scripts/deploy.js
@@ -1,10 +1,11 @@
 async function main() {
-  const NFTMarket = await hre.ethers.getContractFactory("NFTMarket");
-  const nftMarket = await NFTMarket.deploy();
-  await nftMarket.deployed();
-  console.log("nftMarket deployed to:", nftMarket.address);
+  console.log(100);
+  const NFTMarket = await ethers.getContractFactory("NFTMarket")
+  const nftmarket = await NFTMarket.deploy()
+  await nftmarket.deployed()
+  console.log("Contract deployed to address:", nftmarket.address)
 
-  const NFT = await hre.ethers.getContractFactory("NFT");
+  const NFT = await ethers.getContractFactory("NFT");
   const nft = await NFT.deploy(nftMarket.address);
   await nft.deployed();
   console.log("nft deployed to:", nft.address);
