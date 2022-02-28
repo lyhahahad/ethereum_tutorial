299212fb9dc47d9b3f3f9d9c5c2fdded4658303a
lyh <dltbwoddl@gmail.com>
2022-02-28 11:18:59 +0900

    full stack dapp contract 분석

--- fullstackdapp/digital-marketplace/contracts/Market.sol
+++ fullstackdapp/digital-marketplace/contracts/Market.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
-
+// counter : 증가 또는 감소만 가능한 카운터를 얻는 간단한 모듈.
 // ReentrancyGuard :  재진입 호출을 방지하는 데 도움이 되는 계약 모듈입니다.
 
 contract NFTMarket is ReentrancyGuard {
@@ -32,6 +32,7 @@ contract NFTMarket is ReentrancyGuard {
 
   mapping(uint256 => MarketItem) private idToMarketItem;
 
+// event는 트랜잭션 영수증이라고 보면 된다.
   event MarketItemCreated (
     uint indexed itemId,
     address indexed nftContract,
@@ -42,6 +43,7 @@ contract NFTMarket is ReentrancyGuard {
     bool sold
   );
 
+// msg.sender를 명시적으로 payable(지불가능한)로 캐스트한다.
   constructor() {
     owner = payable(msg.sender);
   }
@@ -86,6 +88,13 @@ contract NFTMarket is ReentrancyGuard {
     );
   }
 
+  /*
+  기능 : 구매자가 해당 아이템을 사고 싶을 때 발생하는 함수.
+  변경하는 전역 변수 : idToMarketItem(itemId-> MarketItem mapping변수이다.)
+  _itemsSold(카운터 변수로 마켓에서 팔린 총 아이템 개수를 반환한다.)
+  에러
+  1.구매자가 보낸 message에 담겨있는 value 변수가 판매자가 등록한 price와 같지 않을 떄.
+  */
   /* Creates the sale of a marketplace item */
   /* Transfers ownership of the item, as well as funds between parties */
   function createMarketSale(
@@ -96,11 +105,16 @@ contract NFTMarket is ReentrancyGuard {
     uint tokenId = idToMarketItem[itemId].tokenId;
     require(msg.value == price, "Please submit the asking price in order to complete the purchase");
 
+    // seller에게 msg.value만큼의 이더를 보낸다?
     idToMarketItem[itemId].seller.transfer(msg.value);
+    //nft를 보내는 코드.
     IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+    //오너 변경 및 판매 등록 취소
     idToMarketItem[itemId].owner = payable(msg.sender);
     idToMarketItem[itemId].sold = true;
     _itemsSold.increment();
+    //listing price를 owner에게 listingprice 만큼의 이더를 보낸다?
+    // owner와 seller의 차이는?
     payable(owner).transfer(listingPrice);
   }
 
