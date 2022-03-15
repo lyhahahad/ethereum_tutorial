283d57cc15a1475bafe93351d42286ec9435e4b8
lyh <glawkdxn0328@naver.com>
2022-03-15 14:42:26 +0900

    상속성 공부

---
+++ solidity-smartcontract/basic-grammer/inheritance.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/**
+ solidity는 단일 상속, 다중 상속을 모두 지원한다.
+ derivaed contract는 internal methods를 포함한 non-private, 상태 변수에 접근할 수 있다. 하지만 사용은 불가능하다.
+ function overriding은 signature가 동일하면 허용된다.
+ */
